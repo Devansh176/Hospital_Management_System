a02261f9def521b85b9e2fad521f42efca237d88
Devansh <devanshdhopte@gmail.com>
2025-11-26 15:14:05 +0530

    Added some test cases in test folder

--- src/main/java/com/project/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/project/hospitalManagement/repository/PatientRepository.java
@@ -1,10 +1,32 @@
 package com.project.hospitalManagement.repository;
 
 import com.project.hospitalManagement.entity.Patient;
+import com.project.hospitalManagement.type.BloodGroupType;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    Patient findByName(String name);
+    Patient findByBirthDate(LocalDate birthDate);
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+    List<Patient> findByNameContaining(String query);
+
+    //for more type of queries, visit:
+    //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+
+    //This is JPQL - Jakarta Persistence Query Language
+    //JPQL is used when you want to write the query by yourself without predefined queries.
+    @Query("Select p from Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
+
+    @Query("select p from Patient p ")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
 }
--- src/test/java/com/project/hospitalManagement/PatientTests.java
+++ src/test/java/com/project/hospitalManagement/PatientTests.java
@@ -3,10 +3,12 @@ package com.project.hospitalManagement;
 import com.project.hospitalManagement.entity.Patient;
 import com.project.hospitalManagement.repository.PatientRepository;
 import com.project.hospitalManagement.service.PatientService;
+import com.project.hospitalManagement.type.BloodGroupType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -26,8 +28,19 @@ public class PatientTests {
 
     @Test
     public void testTransactionMethods() {
-        Patient patient = patientService.getStudentById(1L);
-        System.out.println(patient);
+//        Patient patient = patientService.getStudentById(1L);
+//        Patient patient = patientRepository.findByName("Rohan Patel");
 
+        List<Patient> patients = patientRepository
+                .findByBirthDateOrEmail(
+                        LocalDate.of(1998,5,14),
+                        "aarav.sharma@example.com"
+                );
+
+//        List<Patient> patientList = patientRepository.findByNameContaining("ma");
+//        for(Patient pt : patientList) System.out.println(pt);
+
+        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+        System.out.println(patientList);
     }
 }
